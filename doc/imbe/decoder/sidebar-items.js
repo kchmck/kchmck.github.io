5786fe66f82268806ebebc4db35baba6623e0b7a
Mick Koch <mick@kochm.co>
2018-01-01 21:37:22 -0500

    Update rust docs for real

--- doc/imbe/decoder/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["IMBEDecoder","Decodes a stream of IMBE frames."]]});
--- doc/implementors/static_fir/trait.FirCoefs.js
+++ doc/implementors/static_fir/trait.FirCoefs.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["p25"] = ["impl <a class=\"trait\" href=\"static_fir/trait.FirCoefs.html\" title=\"trait static_fir::FirCoefs\">FirCoefs</a> for <a class=\"struct\" href=\"p25/baseband/sync/struct.SyncFingerprint.html\" title=\"struct p25::baseband::sync::SyncFingerprint\">SyncFingerprint</a>",];
+implementors["p25"] = ["impl FirCoefs for <a class=\"struct\" href=\"p25/baseband/sync/struct.SyncFingerprint.html\" title=\"struct p25::baseband::sync::SyncFingerprint\">SyncFingerprint</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
